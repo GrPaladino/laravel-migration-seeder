8f48b320d16def0d9d5f3cde4f66f56089622b3d
GrPaladino <paladino.graziano@gmail.com>
2024-03-20 17:23:46 +0100

    Feat: add migration for table update

---
+++ database/migrations/2024_03_20_161954_move_price_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('price')->after('estimate_arrive_date_time')->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('price')->after('deleted');
+
+        });
+    }
+};
